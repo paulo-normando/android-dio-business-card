661243b7b0d104639b1e9cf5ff5e21cb40f48eaf
Paulo Normando <paulonormando@gmail.com>
2022-07-15 16:34:22 -0300

    Manifest atualizado para incluir Activity de Ui AddBusinessCardActivity.kt

--- app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,20 @@ package br.com.dio.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import br.com.dio.businesscard.App
+import br.com.dio.businesscard.R
+import br.com.dio.businesscard.data.BusinessCard
 import br.com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    private val mainVIewModel: MainVIewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
         binding.btnConfirmar.setOnClickListener {
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                empresa = binding.tilTelefone.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString()
+            )
+            mainVIewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
